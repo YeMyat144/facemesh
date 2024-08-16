61523586ce966d0eebe303964ed28132144c0bf2
Ye Myat Moe <149359862+YeMyat144@users.noreply.github.com>
2024-08-16 11:18:05 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, {useRef, useState} from 'react';
 import './App.css';
+import * as tf from '@tensorflow/tfjs';
 import * as facemesh from '@tensorflow-models/facemesh';
 import Webcam from 'react-webcam';
-import { drawMesh } from './utilities';
+import {drawMesh} from './utilities';
 
 function App() {
   // Setup references
@@ -13,30 +14,23 @@ function App() {
   const [isCameraOn, setIsCameraOn] = useState(false);
 
   // Load facemesh model
-  useEffect(() => {
-    const runFacemesh = async () => {
-      const net = await facemesh.load({
-        inputResolution: { width: 640, height: 480 },
-        scale: 0.8
-      });
-      const interval = setInterval(() => {
-        if (isCameraOn) {
-          detect(net);
-        }
-      }, 100);
-
-      // Cleanup function
-      return () => clearInterval(interval);
-    };
-
-    runFacemesh();
-  }, [isCameraOn]);
+  const runFacemesh = async () => {
+    const net = await facemesh.load({
+      inputResolution: { width: 640, height: 480 },
+      scale: 0.8
+    });
+    setInterval(() => {
+      if (isCameraOn) {
+        detect(net);
+      }
+    }, 100);
+  }
 
   // Detect function
   const detect = async (net) => {
     if (
-      typeof webcamRef.current !== 'undefined' &&
-      webcamRef.current !== null &&
+      typeof webcamRef.current !== "undefined" && 
+      webcamRef.current !== null && 
       webcamRef.current.video.readyState === 4
     ) {
       // Get Video Properties
@@ -57,8 +51,8 @@ function App() {
       console.log(face);
 
       // Get canvas context for drawing
-      const ctx = canvasRef.current.getContext('2d');
-      requestAnimationFrame(() => { drawMesh(face, ctx); });
+      const ctx = canvasRef.current.getContext("2d");
+      requestAnimationFrame(() => { drawMesh(face, ctx) });
     }
   }
 
@@ -67,16 +61,43 @@ function App() {
     setIsCameraOn(prevState => !prevState);
   }
 
+  runFacemesh();
+
   return (
     <div className="App">
       <header className="App-header">
         {isCameraOn && (
-          <div className="Webcam-container">
-            <Webcam ref={webcamRef} style={{ width: '100%', height: '100%' }} />
-            <canvas ref={canvasRef} className="canvas" />
-          </div>
+          <>
+            <Webcam ref={webcamRef} style={{
+              position: 'absolute',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              left: 0,
+              right: 0,
+              textAlign: 'center',
+              zIndex: 9,
+              width: 640,
+              height: 480
+            }} />
+            <canvas ref={canvasRef} style={{
+              position: 'absolute',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              left: 0,
+              right: 0,
+              textAlign: 'center',
+              zIndex: 9,
+              width: 640,
+              height: 480
+            }} />
+          </>
         )}
-        <button onClick={toggleCamera}>
+        <button onClick={toggleCamera} style={{
+          position: 'relative',
+          zIndex: 10,
+          marginTop: isCameraOn ? '550px' : '20px',
+          marginBottom: isCameraOn ? '20px' : '0px',
+        }}>
           {isCameraOn ? 'Turn Camera Off' : 'Turn Camera On'}
         </button>
       </header>
